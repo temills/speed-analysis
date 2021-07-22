a2c1489b12c9fe2206e26d14be8183d6b22f16ba
temills <63208081+temills@users.noreply.github.com>
2021-07-22 12:01:27 -0400

    chain restaurants

--- parseData.js
+++ parseData.js
@@ -72,7 +72,7 @@
         console.log("Parsing data");
         data = reformatData(data);
         console.log("done");
-        var table = 'vegetables';
+        var table = 'chain_restaurants';
         var keys = "";
         var keyArr = Object.keys(data[0]);
         for(var i=0; i<keyArr.length; i++) {
